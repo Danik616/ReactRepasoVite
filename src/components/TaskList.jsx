c87130ceef8c43c732182b65d22572af8e998b3c
Danik616 <ddanielvargas2011@gmail.com>
2023-05-17 13:01:46 -0500

    Se crea el sistema para la eliminacion de los datos

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,13 +1,13 @@
 import TaskCard from "./TaskCard";
 
-function Tasklist({ tasks }) {
+function Tasklist({ tasks, deleteTask }) {
   if (tasks.length === 0) {
     return <h1>No hay tareas aún</h1>;
   }
   return (
     <div>
       {tasks.map((task) => (
-        <TaskCard task={task} key={task.id} />
+        <TaskCard task={task} key={task.id} deleteTask={deleteTask}/>
       ))}
     </div>
   );
